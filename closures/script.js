3ff0fc8e061b475b8effb1290986a319ee45bdd6
Mario Lazzari <mario.lazzari@gmail.com>
2021-08-26 21:35:42 +0200

    Added closures

--- closures/script.js
+++ closures/script.js
@@ -9,3 +9,26 @@ printName();
 
 myName = "Maria";
 printName();
+
+// inner function
+function outerFunction(outerValue) {
+  const outer2 = "outer2";
+
+  return function innerFunction(innerValue) {
+    console.log("outerValue", outerValue);
+    console.log("innerValue", innerValue);
+    console.log(outer2);
+  };
+}
+
+const newFunction = outerFunction("outside");
+newFunction("inner");
+
+// ex
+function getUrl(url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+
+getUrl("https://jsonplaceholder.typicode.com/posts");
