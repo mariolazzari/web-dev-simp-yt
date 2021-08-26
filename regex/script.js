d5518a4556fc31aff391d83c712419d267ee013c
Mario Lazzari <mario.lazzari@gmail.com>
2021-08-26 22:02:28 +0200

    Added regex

---
+++ regex/script.js
@@ -0,0 +1,11 @@
+const str =
+  "The fat cat ran down the street. It was searching for a mouse to eat";
+
+let regex = /cat/g;
+
+let res = str.match(regex);
+console.log(regex, res);
+
+regex = /at/g;
+res = str.match(regex);
+console.log(regex, res);
