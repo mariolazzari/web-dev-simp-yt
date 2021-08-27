bc7b4c57f1883e48736811927b2b9528da2220c8
Mario Lazzari <mario.lazzari@gmail.com>
2021-08-27 22:26:03 +0200

    Added css positioning

--- regex/script.js
+++ regex/script.js
@@ -2,10 +2,13 @@ const str =
   "The fat cat ran down the street. It was searching for a mouse to eat";
 
 let regex = /cat/g;
-
 let res = str.match(regex);
 console.log(regex, res);
 
 regex = /at/g;
+res = str.match(regex);
+console.log(regex, res);
+
+regex = /at/;
 res = str.match(regex);
 console.log(regex, res);
